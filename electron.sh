dcaa6b1a03c8280789b7b200f877db05df0e60cf
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-18 11:27:51 +0200

    Upload

---
+++ electron.sh
@@ -0,0 +1,163 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Variable pour déterminer si gum doit être utilisé
+USE_GUM=true
+
+# Vérification des arguments
+for arg in "$@"; do
+    case $arg in
+        --nogum|-ng)
+            USE_GUM=false
+            shift
+            ;;
+    esac
+done
+
+# Fonction pour vérifier et installer gum
+check_and_install_gum() {
+    if $USE_GUM && ! command -v gum &> /dev/null; then
+        echo "Installation de gum..."
+        pkg install gum -y
+    fi
+}
+
+# Fonction pour afficher la bannière avec ou sans gum
+show_banner() {
+    clear
+    if $USE_GUM; then
+        gum style \
+            --foreground 212 \
+            --border-foreground 212 \
+            --align center \
+            --width 40 \
+            --margin "1 2" \
+            "Oh-My-Termux" \
+            "Electron Node.js"
+    else
+        echo "Oh-My-Termux"
+        echo "Electron Node.js"
+    fi
+}
+
+# Appel de la fonction pour vérifier et installer gum
+check_and_install_gum
+
+# Afficher la bannière
+show_banner
+
+# Désinstallation si le paramètre --uninstall ou -u est passé
+if [[ "$1" == "--uninstall" || "$1" == "-u" ]]; then
+    show_banner
+    if $USE_GUM; then
+        if gum confirm "Désinstaller Electron Node.js ?"; then
+            echo "Désinstallation d'Electron Node.js ..."
+            gum spin --spinner dot --title "Désinstallation de Node.js ..." -- pkg uninstall nodejs nodejs-lts -y
+            gum spin --spinner dot --title "Désinstallation d'Electron ..." -- pkg uninstall electron -y
+            # Supprimer le fichier de configuration des variables d'environnement
+            if [ -f "$PREFIX/etc/profile.d/electron-nodejs.sh" ]; then
+                rm "$PREFIX/etc/profile.d/electron-nodejs.sh"
+                echo "Fichier de configuration des variables supprimé."
+            fi
+            echo "Désinstallation terminée !"
+        fi
+    else
+        read -p "Désinstaller Electron Nodejs ? (o/n) " confirm
+        if [[ $confirm =~ ^[Oo]$ ]]; then
+            echo "Désinstallation d'Electron Node.js ..."
+            pkg uninstall nodejs nodejs-lts -y
+            pkg uninstall electron -y
+            # Supprimer le fichier de configuration des variables d'environnement
+            if [ -f "$PREFIX/etc/profile.d/electron-nodejs.sh" ]; then
+                rm "$PREFIX/etc/profile.d/electron-nodejs.sh"
+                echo "Fichier de configuration des variables supprimé."
+            fi
+            echo "Désinstallation terminée !"
+        fi
+    fi
+    exit 0
+fi
+
+echo ""
+if $USE_GUM; then
+    if gum confirm "Installer Electron Node.js dans Termux ?"; then
+        if [ "$(getprop ro.build.version.release)" -lt 7 ]; then
+            echo "Les versions Android inférieures à 7 ne sont pas prises en charge !"
+            exit 1
+        fi
+        gum spin --spinner dot --title "Mise à jour des paquets ..." -- pkg upgrade -y
+        gum spin --spinner dot --title "Installation des dépôts requis ..." -- pkg install tur-repo x11-repo -y
+        clear
+        show_banner
+        echo ""
+        version=$(gum choose --limit 1 --height=5 "nodejs" "nodejs-lts")
+        case $version in
+            "nodejs")
+                gum spin --spinner dot --title "Installation de nodejs ..." -- pkg install nodejs -y
+                ;;
+            "nodejs-lts")
+                gum spin --spinner dot --title "Installation de nodejs-lts ..." -- pkg install nodejs-lts -y
+                ;;
+            *)
+                echo "Erreur !" && exit
+                ;;
+        esac
+        clear
+        show_banner
+        echo ""
+        gum spin --spinner dot --title "Installation d'Electron..." -- pkg install electron -y
+        echo "Modification des variables..."
+        cat <<EOF > $PREFIX/etc/profile.d/electron-nodejs.sh
+#!/data/data/com.termux/files/usr/bin/bash
+export ELECTRON_SKIP_BINARY_DOWNLOAD=1
+export ELECTRON_OVERRIDE_DIST_PATH=/data/data/com.termux/files/usr/bin
+EOF
+        source $PREFIX/etc/profile.d/electron-nodejs.sh
+        echo "Installation d'Electron Node.js terminée !"
+    fi
+else
+    read -p "Installer Electron Node.js dans Termux ? (o/n) " confirm
+    if [[ $confirm =~ ^[Oo]$ ]]; then
+        if [ "$(getprop ro.build.version.release)" -lt 7 ]; then
+            echo "Les versions Android inférieures à 7 ne sont pas prises en charge !"
+            exit 1
+        fi
+        echo "Mise à jour des paquets ..."
+        pkg upgrade -y
+        echo "Installation des dépôts requis ..."
+        pkg install tur-repo x11-repo -y
+        clear
+        show_banner
+        echo ""
+        echo "Sélectionner la version de nodejs à installer :"
+	echo ""
+        echo "1) nodejs"
+        echo "2) nodejs-lts"
+	echo ""
+        read -p "Choix : " version
+        case $version in
+            1)
+                echo "Installation de nodejs ..."
+                pkg install nodejs -y
+                ;;
+            2)
+                echo "Installation de nodejs-lts ..."
+                pkg install nodejs-lts -y
+                ;;
+            *)
+                echo "Erreur !" && exit
+                ;;
+        esac
+        clear
+        show_banner
+        echo ""
+        echo "Installation d'Electron ..."
+        pkg install electron -y
+        cat <<EOF > $PREFIX/etc/profile.d/electron-nodejs.sh
+#!/data/data/com.termux/files/usr/bin/bash
+export ELECTRON_SKIP_BINARY_DOWNLOAD=1
+export ELECTRON_OVERRIDE_DIST_PATH=/data/data/com.termux/files/usr/bin
+EOF
+        source $PREFIX/etc/profile.d/electron-nodejs.sh
+        echo "Installation d'Electron Node.js terminée !"
+    fi
+fi
---
+++ usersymlink.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+
+# Variable pour déterminer si gum doit être utilisé
+USE_GUM=true
+
+# Vérification des arguments
+for arg in "$@"; do
+    case $arg in
+        --nogum|-ng)
+            USE_GUM=false
+            shift
+            ;;
+    esac
+done
+
+# Fonction pour vérifier et installer gum
+check_and_install_gum() {
+    if $USE_GUM && ! command -v gum &> /dev/null; then
+        echo "gum n'est pas installé. Installation en cours..."
+        pkg update -y && pkg install gum -y
+    fi
+}
+
+# Fonction pour afficher la bannière
+show_banner() {
+    clear
+    if $USE_GUM; then
+        gum style \
+            --foreground 212 \
+            --border-foreground 212 \
+            --border double \
+            --align center \
+            --width 40 \
+            --margin "1 2" \
+            --padding "1 1" \
+            "Oh-My-Termux" \
+            "User Symlink" \
+	    ""
+    else
+        echo -e "\e[1;34mOh-My-Termux\e[0m"
+        echo -e "\e[1;35mUser Symlink\e[0m"
+	      echo
+    fi
+}
+
+# Fonction pour afficher un message d'erreur
+display_error_message() {
+    clear
+    show_banner
+    echo -e "\e[1;31mErreur de saisie. Veuillez recommencer.\e[0m"  # Texte en rouge gras
+    echo
+}
+
+# Exécuter la vérification et l'installation de gum si nécessaire
+check_and_install_gum
+
+# Vérification de l'exécution de termux-setup-storage
+if [ ! -d "$HOME/storage" ]; then
+    echo "Exécution de termux-setup-storage..."
+    termux-setup-storage
+fi
+
+# Afficher la bannière
+show_banner
+
+# Fonction pour lister les liens symboliques existants
+list_symlinks() {
+    symlinks=()
+    for item in "$HOME"/*; do
+        if [ -L "$item" ]; then
+            symlinks+=("$(basename "$item")")
+        fi
+    done
+}
+
+# Vérifier si tous les liens symboliques ont été créés
+check_all_symlinks_created() {
+    list_symlinks
+    directories=("📂 Téléchargement" "🖼️ Images" "📸 Photos" "🎥 Vidéos" "🎵 Musique" "📄 Documents" "📁 Stockage Interne")
+    created_symlinks=0
+
+    for dir in "${directories[@]}"; do
+        if [[ " ${symlinks[@]} " =~ " ${dir} " ]]; then
+            ((created_symlinks++))
+        fi
+    done
+
+    if [ $created_symlinks -eq ${#directories[@]} ]; then
+        clear
+        show_banner
+        echo "Tous les liens symboliques ont été créés."
+        echo
+	exit 0
+    fi
+}
+
+# Fonction pour gérer la suppression des liens symboliques
+delete_symlinks() {
+    list_symlinks
+    symlinks+=("Tous les répertoires" "Terminer")  # Ajouter les options
+    while true; do
+        if [ ${#symlinks[@]} -eq 2 ]; then
+            clear
+            show_banner
+            echo "Aucun lien symbolique à supprimer."
+            break
+        fi
+        echo "Suppression de lien symbolique"
+        echo
+        if $USE_GUM; then
+            selected_symlinks=$(printf "%s\n" "${symlinks[@]}" | gum choose --limit 1 --height=11)
+        else
+            for i in "${!symlinks[@]}"; do
+            echo "$((i+1))) ${symlinks[i]}"
+            done
+	    echo
+            read -p "Entrez le numéro de votre choix : " choice
+            selected_symlinks="${symlinks[$((choice-1))]}"
+        fi
+        if [ -n "$selected_symlinks" ]; then
+            if [[ "$selected_symlinks" == "Terminer" ]]; then
+                clear
+		show_banner
+		echo "Fin de la suppression."
+                break
+            elif [[ "$selected_symlinks" == "Tous les répertoires" ]]; then
+                clear
+		show_banner
+		echo "Tous les liens symboliques ont été supprimé."
+                echo
+		for link in "${symlinks[@]}"; do
+                    if [[ "$link" != "Terminer" && "$link" != "Tous les répertoires" ]]; then
+                        rm "$HOME/$link"
+                    fi
+                done
+                exit 0  # Terminer le script après suppression
+            else
+                echo "Suppression du lien symbolique : $selected_symlinks"
+                rm "$HOME/$selected_symlinks"
+                symlinks=("${symlinks[@]/$selected_symlinks}")
+            fi
+            clear
+            show_banner
+        fi
+    done
+}
+
+# Mode suppression
+if [[ "$1" == "--delete" || "$1" == "-d" ]]; then
+    delete_symlinks
+    exit 0
+fi
+
+# Vérifier si tous les liens symboliques ont déjà été créés au démarrage
+check_all_symlinks_created
+
+# Liste des répertoires possibles
+directories=("📂 Téléchargement" "🖼️ Images" "📸 Photos" "🎥 Vidéos" "🎵 Musique" "📄 Documents" "📁 Stockage Interne")
+
+# Fonction pour afficher et gérer la sélection des répertoires
+display_directories() {
+    list_symlinks
+    filtered_directories=()
+    for dir in "${directories[@]}"; do
+        if [[ ! " ${symlinks[@]} " =~ " ${dir} " ]]; then
+            filtered_directories+=("$dir")
+        fi
+    done
+    filtered_directories+=("Tous les répertoires" "Terminer")
+
+    while true; do
+        echo "Création de lien symbolique"
+        echo
+        if $USE_GUM; then
+            selected_dirs=$(printf "%s\n" "${filtered_directories[@]}" | gum choose --limit 1 --height=11)
+        else
+    	    for i in "${!filtered_directories[@]}"; do
+                echo "$((i+1))) ${filtered_directories[i]}"
+            done
+            echo
+	   read -p "Entrez le numéro de votre choix : " choice
+            if ! [[ "$choice" =~ ^[0-9]+$ ]] || [ "$choice" -lt 1 ] || [ "$choice" -gt "${#filtered_directories[@]}" ]; then
+                display_error_message
+                continue
+            fi
+             selected_dirs="${filtered_directories[$((choice-1))]}"
+        fi
+        if [ -n "$selected_dirs" ]; then
+            if [[ "$selected_dirs" == "Terminer" ]]; then
+                clear
+		show_banner
+		echo "Script terminé !"
+                echo
+		exit 0
+            elif [[ "$selected_dirs" == "Tous les répertoires" ]]; then
+                for dir in "${directories[@]}"; do
+                    if [[ ! " ${symlinks[@]} " =~ " ${dir} " ]]; then
+                        case $dir in
+                            "📂 Téléchargement")
+                                ln -s "$HOME/storage/downloads" "$HOME/📂 Téléchargement"
+                                ;;
+                            "🖼️ Images")
+                                ln -s "$HOME/storage/pictures" "$HOME/🖼️ Images"
+                                ;;
+                            "📸 Photos")
+                                ln -s "$HOME/storage/dcim" "$HOME/📸 Photos"
+                                ;;
+                            "🎥 Vidéos")
+                                ln -s "$HOME/storage/movies" "$HOME/🎥 Vidéos"
+                                ;;
+                            "🎵 Musique")
+                                ln -s "$HOME/storage/music" "$HOME/🎵 Musique"
+                                ;;
+                            "📄 Documents")
+                                ln -s "$HOME/storage/documents" "$HOME/📄 Documents"
+                                ;;
+                            "📁 Stockage Interne")
+                                ln -s "$HOME/storage/shared" "$HOME/📁 Stockage Interne"
+                                ;;
+                        esac
+                        check_all_symlinks_created  # Vérification après chaque création
+                    fi
+                done
+                exit 0  # Terminer le script après création
+            else
+                echo "Création de lien symbolique pour : $selected_dirs"
+                case $selected_dirs in
+                    "📂 Téléchargement")
+                        ln -s "$HOME/storage/downloads" "$HOME/📂 Téléchargement"
+                        ;;
+                    "🖼️ Images")
+                        ln -s "$HOME/storage/pictures" "$HOME/🖼️ Images"
+                        ;;
+                    "📸 Photos")
+                        ln -s "$HOME/storage/dcim" "$HOME/📸 Photos"
+                        ;;
+                    "🎥 Vidéos")
+                        ln -s "$HOME/storage/movies" "$HOME/🎥 Vidéos"
+                        ;;
+                    "🎵 Musique")
+                        ln -s "$HOME/storage/music" "$HOME/🎵 Musique"
+                        ;;
+                    "📄 Documents")
+                        ln -s "$HOME/storage/documents" "$HOME/📄 Documents"
+                        ;;
+                    "📁 Stockage Interne")
+                        ln -s "$HOME/storage/shared" "$HOME/📁 Stockage Interne"
+                        ;;
+                esac
+                check_all_symlinks_created  # Vérification après chaque création
+                filtered_directories=("${filtered_directories[@]/$selected_dirs}")
+            fi
+            clear
+            show_banner
+        fi
+    done
+}
+
+# Boucle principale
+display_directories
+
+# Effacer le terminal et afficher la bannière avant de terminer
+clear
+show_banner
+
+if $USE_GUM; then
+    clear
+    show_banner
+    gum style --foreground 212 --bold "Script terminé !"
+else
+    clear
+    show_banner
+    echo "Script terminé !"
+fi
